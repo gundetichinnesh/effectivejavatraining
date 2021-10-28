7f2ce159ed5ad582750b6a7a364ffacfa77e90aa
gundetichinnesh <49790616+gundetichinnesh@users.noreply.github.com>
2021-10-28 08:36:50 +0530

    Assignment 3

---
+++ AccountApp.java
@@ -0,0 +1,12 @@
+package multipleaccount;
+
+public class AccountApp {
+
+	public static void main(String[] args) {
+		SavingsAccount a1=new SavingsAccount();
+		a1.createaccount();
+		// TODO Auto-generated method stub
+
+	}
+
+}
